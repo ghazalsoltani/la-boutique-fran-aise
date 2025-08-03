0e3a59d2e4146b672c3536e0713bab67865adc5e
Ghazal <ghazal.soltaninasab@gmail.com>
2025-08-04 00:28:02 +0200

    secure admin backoffice access with authentication

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -31,6 +32,10 @@ class UserCrudController extends AbstractCrudController
             //Generate name and family
             TextField::new('firstname')->setLabel('Prénom'),
             TextField::new('lastname')->setLabel('Nom'),
+            ChoiceField::new('roles')->setLabel('Permissions')->setHelp('Vous pouvez choisir le rôle de cet utilisateur')->setChoices([
+                'ROLE_USER' => 'ROLE_USER',
+                'ROLE_ADMIN' => 'ROLE_ADMIN',
+            ])->allowMultipleChoices(),
             TextField::new('email')->setLabel('Email')->onlyOnIndex()
 
         ];
